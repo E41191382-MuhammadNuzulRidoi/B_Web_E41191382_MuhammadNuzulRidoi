eeff1654df90bc653ad2ac5838015d1721af6247
E41191382-MuhammadNuzulRidoi <nuzulridoi25@gmail.com>
2021-03-25 21:20:07 +0700

    tugas ppl

---
+++ Minggu3_P3/BELAJARLARAVEL2/app/Http/Controllers/ManagementUserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ManagementUserController extends Controller
+{
+    public function index(){
+        // return "Halo ini adalah method index, dalam controller ManagementUser.";
+        // return "Halo ini nantinya akan digunakan untuk mengambil semua data user.";
+        $nama = "Arvita Agus Kurniasari";
+
+        $pelajaran = ["Algoritma & Pemrograman", "Kalkulus", "Pemrograman Web"];
+
+        return view('home', compact('nama', 'pelajaran'));
+    }
+
+}
---
+++ Minggu3_P3/BELAJARLARAVEL2/routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ManagementUserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('user', [ManagementUserController::class, 'index']);
+Route::post('user', [ManagementUserController::class, 'store']);
+Route::get('user/create', [ManagementUserController::class, 'create']);
+Route::get('user/{id}', [ManagementUserController::class, 'show']);
+Route::put('user/{id}', [ManagementUserController::class, 'update']);
+Route::delete('user/{id}', [ManagementUserController::class, 'destroy']);
+Route::get('user/{id}/edit', [ManagementUserController::class, 'edit']);
